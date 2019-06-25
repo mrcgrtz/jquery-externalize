adec6a51b2ad55711b288c8e64e65f87d35209da
Marc Görtz <mail@marcgoertz.de>
2019-06-25 21:33:13 +0200

    Fix tests.

--- test/index.js
+++ test/index.js
@@ -1,53 +1,52 @@
-/* eslint-env qunit, es6 */
-const { test } = QUnit;
+/* eslint-env qunit */
 
-QUnit.module( "$.externalize()", () => {
+QUnit.module( "$.externalize()", function() {
 
-	test( "With defaults", t => {
-		const link = document.querySelector( "#qunit-fixture a" );
-		t.equal( link.target, "" );
-		t.equal( link.rel, "" );
-		t.equal( link.title, "example.com" );
+	QUnit.test( "With defaults", function( assert ) {
+		var link = document.querySelector( "#qunit-fixture a" );
+		assert.equal( link.target, "" );
+		assert.equal( link.rel, "" );
+		assert.equal( link.title, "example.com" );
 		$( "a[href*='//']" ).externalize();
-		t.equal( link.target, "external" );
-		t.equal( link.rel, "external" );
-		t.equal( link.title, "example.com (Opens in a new window)" );
+		assert.equal( link.target, "external" );
+		assert.equal( link.rel, "external" );
+		assert.equal( link.title, "example.com (Opens in a new window)" );
 	} );
 
-	test( "With custom target window name (name property)", t => {
-		const link = document.querySelector( "#qunit-fixture a" );
-		t.equal( link.target, "", "Target window name is empty." );
+	QUnit.test( "With custom target window name (name property)", function( assert ) {
+		var link = document.querySelector( "#qunit-fixture a" );
+		assert.equal( link.target, "", "Target window name is empty." );
 		$( "a[href*='//']" ).externalize( {
 			name: "test"
 		} );
-		t.equal( link.target, "test", "Target window name is 'test'." );
+		assert.equal( link.target, "test", "Target window name is 'test'." );
 	} );
 
-	test( "With event behavior (target property)", t => {
-		const link = document.querySelector( "#qunit-fixture a" );
-		t.equal( link.target, "" );
+	QUnit.test( "With event behavior (target property)", function( assert ) {
+		var link = document.querySelector( "#qunit-fixture a" );
+		assert.equal( link.target, "" );
 		$( "a[href*='//']" ).externalize( {
 			target: false
 		} );
-		t.equal( link.target, "" );
+		assert.equal( link.target, "" );
 	} );
 
-	test( "With custom link relation (relation property)", t => {
-		const link = document.querySelector( "#qunit-fixture a" );
-		t.equal( link.rel, "", "Link relationship is empty." );
+	QUnit.test( "With custom link relation (relation property)", function( assert ) {
+		var link = document.querySelector( "#qunit-fixture a" );
+		assert.equal( link.rel, "", "Link relationship is empty." );
 		$( "a[href*='//']" ).externalize( {
 			relation: "nofollow"
 		} );
-		t.equal( link.rel, "nofollow", "Link relationship is 'nofollow'." );
+		assert.equal( link.rel, "nofollow", "Link relationship is 'nofollow'." );
 	} );
 
-	test( "With custom title (title property)", t => {
-		const link = document.querySelector( "#qunit-fixture a" );
-		t.equal( link.title, "example.com", "Title is 'example.com'." );
+	QUnit.test( "With custom title (title property)", function( assert ) {
+		var link = document.querySelector( "#qunit-fixture a" );
+		assert.equal( link.title, "example.com", "Title is 'example.com'." );
 		$( "a[href*='//']" ).externalize( {
 			title: "öffnet in neuem Fenster"
 		} );
-		t.equal( link.title, "example.com (öffnet in neuem Fenster)",
+		assert.equal( link.title, "example.com (öffnet in neuem Fenster)",
 			"Title is 'example.com (öffnet in neuem Fenster)'." );
 	} );
 
