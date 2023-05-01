fb385841f43355e85642a58c38a4668d85bb1fe6
Marc Görtz <mail@marcgoertz.de>
2023-05-01 17:31:38 +0200

    Split test jobs.

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+	env: {
+		browser: true,
+		jquery: true
+	},
+	extends: [
+		"eslint:recommended",
+		"jquery"
+	]
+};
